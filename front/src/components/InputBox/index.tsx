c1c38a1f45316993dfff058e4facbc122dc8be5a
changseokjo <yulini1022@gmail.com>
2023-09-01 16:59:43 +0900

    study-20230901

--- front/src/components/InputBox/index.tsx
+++ front/src/components/InputBox/index.tsx
@@ -1,47 +1,52 @@
-import React, { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import React, { ChangeEvent, Dispatch, KeyboardEvent, MutableRefObject, SetStateAction, forwardRef, useRef } from 'react';
 import './style.css';
 
-//          interface: Input 상자 컴포넌트 Props            //
-
+//          interface: Input 상자 컴포넌트 Props          //
 interface Props {
-    label: string;
-    type: 'text' | 'password';
-    error: boolean;
-    placeholder: string;
-    value: string;
-    setValue: Dispatch<SetStateAction<string>>;
-    icon?: string;
-    errorMessage?: string;
-    onButtonClick?: () => void;
+  label: string;
+  type: 'text' | 'password';
+  error: boolean;
+  placeholder: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>
+  icon?: string;
+  errorMessage?: string;
+  onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+  onButtonClick?: () => void;
 }
 
+//          component: Input 상자 컴포넌트          //
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+  //          state: Properties          //
+  const { label, type, error, placeholder, value, icon, errorMessage } = props;
+  const { setValue, onKeyDown, onButtonClick } = props;
 
-//          component: Input 상자 컴포넌트         //
-export default function InputBox(props: Props) {
+  //          event handler: input 값 변경 이벤트 처리          //
+  const onInputValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setValue(value);
+  }
+  //          event handler: input 값 변경 이벤트 처리          //
+  const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (!onKeyDown) return;
+    onKeyDown(event);
+  }
 
-    //          state: Properties           //
-    const { label, type, error, placeholder, value, icon, errorMessage } = props;
-    const { setValue, onButtonClick } = props;
+  //          render: Input 상자 렌더링         //
+  return (
+    <div className='inputbox'>
+      <div className='inputbox-label'>{label}</div>
+      <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+        <input ref={ref} className='input' type={type} placeholder={placeholder} value={value} onChange={onInputValueChangeHandler} onKeyDown={onKeyDownHandler} />
+        {onButtonClick !== undefined && (
+          <div className='icon-button' onClick={onButtonClick}>
+            {icon !== undefined && <div className={icon}></div>}
+          </div>
+        )}
+      </div>
+      <div className='inputbox-message'>{errorMessage}</div>
+    </div>
+  )
+});
 
-    //          event handler: input 값 변경 이벤트         //
-    const onInputValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        setValue(value);
-    }
-
-    //          render: Input 상자 렌더링           //
-    return (
-        <div className='inputbox'>
-            <div className='inputbox-label'>{label}</div>
-            <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
-                <input className='input' type={type} placeholder={placeholder} value={value} onChange={onInputValueChangeHandler} />
-                { onButtonClick !== undefined && (
-                <div className='icon-button' onClick={ onButtonClick }>
-                    { icon !== undefined && <div className={icon}></div> }
-                </div>
-                )}
-            </div>
-            <div className='inputbox-message'>{ errorMessage }</div>
-        </div>
-    )
-}
+export default InputBox;
--- front/src/mocks/index.ts
+++ front/src/mocks/index.ts
@@ -1,9 +1,11 @@
 import top3ListMock from './top3-list.mock';
-import currentBoardListMock from './current-board-list.mock';
+import loginInfoMock from './login-info.mock';
 import commentListMock from './comment-list.mock';
+import currentBoardListMock from './current-board-list.mock';
 
 export { 
-    currentBoardListMock,
     top3ListMock,
+    loginInfoMock,
     commentListMock,
-};
+    currentBoardListMock,
+ };
