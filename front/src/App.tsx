9e62ed4d30bbe00a943b8c21408d474da1f76f03
changseokjo <yulini1022@gmail.com>
2023-09-11 20:43:47 +0900

    study-20230911

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         <Route path={BOARD_WRITE_PATH} element={<BoardWrite />} />
         <Route path={BOARD_DETAIL_PATH(':boadrNumber')} element={<BoardDetail />} />
         <Route path={BOARD_UPDATE_PATH(':boadrNumber')} element={<BoardUpdate />} />
-        <Route path={USER_PATH(':email')} element={<User />} />
+        <Route path={USER_PATH(':searchEmail')} element={<User />} />
         <Route path='*' element={<h1>404 Not Found</h1>} />
       </Route>
     </Routes>
---
+++ front/src/mocks/user.mock.ts
@@ -0,0 +1,9 @@
+import { User } from "types";
+
+const userMock: User = {
+    email: 'email@email.com',
+    nickname: '나는주코야키',
+    profileImage: 'https://img.sbs.co.kr/newsnet/etv/upload/2023/08/18/30000869647_500.jpg'
+}
+
+export default userMock;
