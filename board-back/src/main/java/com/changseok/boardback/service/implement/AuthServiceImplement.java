61d0598728d5dea066f67ddf32e6ddb4a525c76a
changseokjo <yulini1022@gmail.com>
2023-09-21 12:51:42 +0900

    2023.09.21.12:50 study

---
+++ board-back/src/main/java/com/changseok/boardback/service/implement/AuthServiceImplement.java
@@ -0,0 +1,10 @@
+package com.changseok.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.changseok.boardback.service.AuthService;
+
+@Service
+public class AuthServiceImplement implements AuthService{
+    
+}
