47b5c2fd49973a45816ad69304629eb95a398d9b
changseokjo <yulini1022@gmail.com>
2023-09-22 13:47:04 +0900

    2023.09.22.13:50 study

--- board-back/src/main/java/com/changseok/boardback/dto/response/ResponseDto.java
+++ board-back/src/main/java/com/changseok/boardback/dto/response/ResponseDto.java
@@ -1,5 +1,8 @@
 package com.changseok.boardback.dto.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +14,15 @@ public class ResponseDto {
     
     private String code;
     private String message;
+
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto result = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> validationFailed() {
+        ResponseDto result = new ResponseDto(ResponseCode.VALIDATION_FAILED, ResponseMessage.VALIDATION_FAILED);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
 
 }
--- board-back/src/main/java/com/changseok/boardback/service/implement/AuthServiceImplement.java
+++ board-back/src/main/java/com/changseok/boardback/service/implement/AuthServiceImplement.java
@@ -2,27 +2,59 @@ package com.changseok.boardback.service.implement;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.changseok.boardback.dto.request.auth.SignUpRequestDto;
 import com.changseok.boardback.dto.response.ResponseDto;
 import com.changseok.boardback.dto.response.auth.SignUpResponseDto;
+import com.changseok.boardback.entity.UserEntity;
+import com.changseok.boardback.repository.UserRepository;
 import com.changseok.boardback.service.AuthService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService {
+
+    private final UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto) {
         
         try {
 
+            String email = dto.getEmail();
+            String nickname = dto.getNickname();
+            String telNumber = dto.getTelNumber();
+
+            boolean hasEmail = userRepository.existsByEmail(email);
+            if (hasEmail) return SignUpResponseDto.duplicateEmail();
+
+            boolean hasNickname = userRepository.existsByNickname(nickname);
+            if (hasNickname) return SignUpResponseDto.duplicateNickname();
+
+            boolean hasTelNumber = userRepository.existsByTelNumber(telNumber);
+            if (hasTelNumber) return SignUpResponseDto.duplicateTelNumber();
+
+            String password = dto.getPassword();
+            String encodedPassword = passwordEncoder.encode(password);
+
+            dto.setPassword(encodedPassword);
+
+            UserEntity userEntity = new UserEntity(dto);
+            userRepository.save(userEntity);
+
         } catch(Exception exception) {
             exception.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto("DBE", "Database Error."));
+            return ResponseDto.databaseError();
         }
 
-        return null;
+        return SignUpResponseDto.success();
 
     }
     
