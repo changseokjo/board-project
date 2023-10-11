fe0ad8e70c615c4c3961e1f04004a12fcdab2d65
changseokjo <yulini1022@gmail.com>
2023-10-11 10:56:20 +0900

    2023.10.11.10:50 study

--- board-back/src/main/java/com/changseok/boardback/service/BoardService.java
+++ board-back/src/main/java/com/changseok/boardback/service/BoardService.java
@@ -10,6 +10,7 @@ import com.changseok.boardback.dto.response.board.GetBoardResponseDto;
 import com.changseok.boardback.dto.response.board.GetCommentListResponseDto;
 import com.changseok.boardback.dto.response.board.GetFavoriteListResponseDto;
 import com.changseok.boardback.dto.response.board.GetLatestBoardListResponseDto;
+import com.changseok.boardback.dto.response.board.GetTop3BoardListResponseDto;
 import com.changseok.boardback.dto.response.board.GetUserBoardListResponseDto;
 import com.changseok.boardback.dto.response.board.IncreaseViewCountResponseDto;
 import com.changseok.boardback.dto.response.board.PatchBoardResponseDto;
@@ -28,6 +29,7 @@ public interface BoardService {
     ResponseEntity<? super GetCommentListResponseDto> getCommentList(Integer boardNumber);
     ResponseEntity<? super GetLatestBoardListResponseDto> getLatestBoardList();
     ResponseEntity<? super GetUserBoardListResponseDto> getUserBoardList(String email);
+    ResponseEntity<? super GetTop3BoardListResponseDto> getTop3BoardList();
 
     ResponseEntity<? super PutFavoriteResponseDto> putFavorite(Integer boardNumber, String email);
 
